e7b589d0037ce685061956318d1c14403739e2d0
Tanarak Chunsanit <tanarak@Tanaraks-MacBook-Pro.local>
2024-12-30 11:25:27 +0700

    fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
@@ -228,31 +227,44 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="w-4/6  m-auto p-5 ">
-        <div className="font-semibold">
-          <p className="text-4xl text-red-400">
-            เริ่มจ้างงานฟรีแลนซ์ง่ายๆ กับ Fasterwork
-          </p>
+      <div className="w-4/6  m-auto p-5 flex justify-between">
+        <div>
+          <div className="font-semibold">
+            <p className="text-4xl text-red-400">
+              เริ่มจ้างงานฟรีแลนซ์ง่ายๆ กับ Fasterwork
+            </p>
+          </div>
+          <div className=" p-2 ">
+            <p className="text-xl  font-semibold flex gap-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M8.625 9.75a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 0 1 .778-.332 48.294 48.294 0 0 0 5.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+                />
+              </svg>
+              แค่ทักแชทก็บรีฟงานฟรีแลนซ์ได้แล้ว
+            </p>
+            <p>ไม่ต้องพิจารณา ไม่ต้องอธิบาย ไม่ต้องตรวจสอบ เพราะเรา Faster🔥</p>
+          </div>
         </div>
-        <div className=" p-2 ">
-          <p className="text-xl  font-semibold flex gap-3">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M8.625 9.75a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 0 1 .778-.332 48.294 48.294 0 0 0 5.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
-              />
-            </svg>
-            แค่ทักแชทก็บรีฟงานฟรีแลนซ์ได้แล้ว
-          </p>
-          <p>ไม่ต้องพิจารณา ไม่ต้องอธิบาย ไม่ต้องตรวจสอบ เพราะเรา Faster🔥</p>
+        <div>
+          {" "}
+          <iframe
+            width="360"
+            height="315"
+            src="https://www.youtube.com/embed/hB7CDrVnNCs?si=xe7cEZ34kTIyXbh6"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+          ></iframe>
         </div>
       </div>
     </>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 import Image from "next/image";
 import Logo from "@/images/logo.png";
+import Link from "next/link";
 
-type Props = {};
-
-export default function Navbar({}: Props) {
+export default function Navbar() {
   return (
     <div className=" bg-white  text-red-400 flex items-center p-2">
-      <a href="/">
+      <Link href="/">
         <div className=" flex items-center">
           <Image src={Logo} width={50} height={50} alt="Logo" />
           <p className=" text-3xl">fasterwork</p>
         </div>
-      </a>
+      </Link>
     </div>
   );
 }
